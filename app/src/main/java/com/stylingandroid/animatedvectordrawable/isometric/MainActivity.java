b369f4d9b05949be33a0362d461d19929dd9480c
Mark Allison <mark@stylingandroid.com>
2016-08-07 16:26:33 +0100

    Adds code for Part 3

--- app/src/main/java/com/stylingandroid/animatedvectordrawable/isometric/MainActivity.java
+++ app/src/main/java/com/stylingandroid/animatedvectordrawable/isometric/MainActivity.java
@@ -1,7 +1,10 @@
 package com.stylingandroid.animatedvectordrawable.isometric;
 
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startAnimation();
+    }
+
+    private void startAnimation() {
+        ImageView imageView = (ImageView) findViewById(R.id.animated_image);
+        if (imageView != null) {
+            startImageViewAnimation(imageView);
+        }
+    }
+
+    private void startImageViewAnimation(ImageView imageView) {
+        Drawable drawable = imageView.getDrawable();
+        if (drawable instanceof Animatable) {
+            startAnimatableAnimation((Animatable) drawable);
+        }
+    }
+
+    private void startAnimatableAnimation(Animatable animatable) {
+        animatable.start();
     }
 }
